1a48cd5c1650cc302e708ecef4efd396aec5e424
effdotsh <ephram@eff.sh>
2020-10-20 00:06:52 -0400

    mouths folder

--- combiner-test.py
+++ combiner-test.py
@@ -15,6 +15,7 @@ The format of opencv is (h, w) and pillow Image is (w, h)
 
 
 facePath = "faces/dumb-smile.png"
+mouthPath = "mouths/smile.png"
 image = cv.imread(facePath, 0)
 print(image.shape)
 width = image.shape[1]
@@ -22,14 +23,14 @@ height = image.shape[0]
 
 # cv.imshow('yeet', image)
 
-mouth = [0, 0]
+mouthPos = [0, 0]
 for w in range(width - 1):
     for h in range(height - 1):
         pixel = image[h, w]
         if pixel != 255 and pixel != 0:
             print(pixel)
-            mouth = [w, h]
-            print(mouth)
+            mouthPos = [w, h]
+            print(mouthPos)
 
 
 
@@ -42,11 +43,11 @@ for w in range(width - 1):
 
 
 face =  Image.open(facePath).convert("RGBA")
-cat = Image.open("cat 1.jpg").convert("RGBA")
+mouth = Image.open(mouthPath).convert("RGBA")
 
 
 
-face.paste(cat, (int(mouth[0] - cat.size[0]/2), int(mouth[1] - cat.size[1]/2)), cat)
+face.paste(mouth, (int(mouthPos[0] - mouth.size[0]/2), int(mouthPos[1] - mouth.size[1]/2)), mouth)
 face.save('reeeee.png',"png")
 
 
