a513805ea8400aaf095ce05aa1a9bd814247cfbc
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2020-10-30 12:29:47 -0400

    can auto-remove generate folder\

--- image-generator.py
+++ image-generator.py
@@ -6,28 +6,60 @@ from PIL import Image
 import sys
 import cv2 as cv
 import numpy as np
+import shutil
 
 
-
-
-
-
+if os.path.isdir('generate'):
+    shutil.rmtree('generate')
 os.system('docker kill gentle')
 os.system('docker rm gentle')
+os.mkdir('generate')
 time.sleep(1)
 docker = Popen(['docker', 'run','--name', 'gentle', '-p', '8765:8765', 'lowerquality/gentle'])
-time.sleep(1)
+time.sleep(3)
 r = os.popen('curl -F "audio=@test-audio-1.mp3" -F "transcript=@test-audio-1.txt" "http://localhost:8765/transcriptions?async=false"').read()
 phoneReference = json.load(open('phonemes.json', encoding='utf8'))
 characters = json.load(open('characters.json', encoding='utf8'))
 
-facePath = "faces/dumb-smile.png"
+facePath = 'faces/dumb-smile.png'
 mouthPath = 'mouths/closed.png'
 
 stamps = json.loads(r)
 counter = 0
 videoList = open('generate/videos.txt', 'w+')
-numW = 0
+
+
+def createVideo(fPath, mPath, xPos, yPos, frame):
+    image = cv.imread(fPath, 0)
+    face =  Image.open(fPath).convert("RGBA")
+    mouth = Image.open(mPath).convert("RGBA")
+    width = image.shape[1]
+    height = image.shape[0]
+    mouthPos = [xPos, yPos]
+    face.paste(mouth, (int(mouthPos[0] - mouth.size[0]/2), int(mouthPos[1] - mouth.size[1]/2)), mouth)
+    face.save("generate/" + str(counter) + '.png')
+    os.popen("ffmpeg -loop 1 -i generate/" + str(frame) + ".png -c:v libx264 -t " + str(round(stamps['words'][0]['start'], 4)) + " -pix_fmt yuv420p -vf scale=1920:1080 generate/" + str(frame) + ".mp4")
+    videoList.write("file '" + str(frame) + ".mp4'\n")
+    return frame + 1
+
+
+
+#Make mouth closed until first phoname
+# image = cv.imread(facePath, 0)
+# face =  Image.open(facePath).convert("RGBA")
+# mouth = Image.open(mouthPath).convert("RGBA")
+# width = image.shape[1]
+# height = image.shape[0]
+# mouthPos = [characters['stupid'][0]['x'], characters['stupid'][0]['y']]
+# face.paste(mouth, (int(mouthPos[0] - mouth.size[0]/2), int(mouthPos[1] - mouth.size[1]/2)), mouth)
+# face.save("generate/" + str(counter) + '.png')
+#
+# os.popen("ffmpeg -loop 1 -i generate/" + str(counter) + ".png -c:v libx264 -t " + str(round(stamps['words'][0]['start'], 4)) + " -pix_fmt yuv420p -vf scale=1920:1080 generate/" + str(counter) + ".mp4")
+# videoList.write("file '" + str(counter) + ".mp4'\n")
+# counter += 1
+counter = createVideo(facePath, mouthPath, characters['stupid'][0]['x'], characters['stupid'][0]['y'], counter)
+
+
 for w in range(len(stamps['words'])):
     word = stamps['words'][w]
     print(word['alignedWord'])
@@ -67,7 +99,6 @@ for w in range(len(stamps['words'])):
         #increase image counter
         counter += 1
     if (w < len(stamps['words']) -1):
-        numW = counter
         mouthPath = 'mouths/closed.png'
         face =  Image.open(facePath).convert("RGBA")
         mouth = Image.open(mouthPath).convert("RGBA")
@@ -79,14 +110,13 @@ for w in range(len(stamps['words'])):
 
         os.popen("ffmpeg -loop 1 -i generate/" + str(counter) + ".png -c:v libx264 -t " + str((round(stamps['words'][w + 1]['start'], 4) - round(stamps['words'][w]['end'], 4))) + " -pix_fmt yuv420p -vf scale=1920:1080 generate/" + str(counter) + ".mp4")
         videoList.write("file '" + str(counter) + ".mp4'\n")
-    #
         counter += 1
-    #     cv.waitKey(0)
-    #     cv.destroyAllWindows()
 
 #Combine all videos into one video
 videoList.flush()
-time.sleep(3)
-
 os.popen("ffmpeg -f concat -safe 0 -i generate/videos.txt -c copy output.mp4").read()
-print(stamps['words'][w]['endOffset']/100)
+time.sleep(1)
+if os.path.isdir('generate'):
+    shutil.rmtree('generate')
+os.system('docker kill gentle')
+os.system('docker rm gentle')
