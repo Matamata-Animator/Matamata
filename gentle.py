55ea502c42df5709bb9e713bd47fd017006b6395
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-01-05 12:06:13 -0500

    finds pose locs

--- gentle.py
+++ gentle.py
@@ -10,7 +10,7 @@ def init():
     command.run('docker kill gentle')
 
     command.run('docker rm gentle')
-    # command.run('docker run --name gentle -p 8765:8765 lowerquality/gentle', False)
+    command.run('docker run --name gentle -p 8765:8765 lowerquality/gentle', False)
 
 
 def clean(gentle):
@@ -27,12 +27,11 @@ def align(audio, text):
     while not os.path.isfile(text):
         pass
 
-    # # get output from gentle
-    print('reeeeeeee')
-
+    # get output from gentle
     url = 'http://localhost:8765/transcriptions?async=false'
     files = {'audio': open(audio, 'rb'),
              'transcript': open('generate/script.txt', 'rb')}
     r = requests.post(url, files=files)
     gentle_out = json.loads(r)
+    print(gentle_out)
     return gentle_out
