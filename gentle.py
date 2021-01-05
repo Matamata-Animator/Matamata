a45c6b81fb98a8d1ace2368eb5273759b0b04d2c
effdotsh <ephram@eff.sh>
2021-01-05 11:42:36 -0500

    uses requests now

--- gentle.py
+++ gentle.py
@@ -2,13 +2,16 @@ import command
 import json
 import pycurl
 from io import BytesIO
+import os
+import requests
 
 
 def init():
     command.run('docker kill gentle')
 
     command.run('docker rm gentle')
-    command.run('docker run --name gentle -p 8765:8765 lowerquality/gentle', False)
+    # command.run('docker run --name gentle -p 8765:8765 lowerquality/gentle', False)
+
 
 def clean(gentle):
     while gentle['words'][0]['case'] == 'not-found-in-audio':
@@ -19,17 +22,17 @@ def clean(gentle):
 
 
 def align(audio, text):
-    # get output from gentle
+    while not os.path.isfile(audio):
+        pass
+    while not os.path.isfile(text):
+        pass
 
-    buffer = BytesIO()
-    g = pycurl.Curl()
-    payload = [('audio', (g.FORM_FILE, audio)), ('transcript', (g.FORM_FILE, text))]
-    g.setopt(pycurl.URL, "http://localhost:8765/transcriptions?async=false")
-    g.setopt(pycurl.HTTPPOST, payload)
-    g.setopt(pycurl.WRITEFUNCTION, buffer.write)
-    g.perform()
-    g.close()
-    res = buffer.getvalue().decode("utf-8")
-    buffer.close()
-    gentle_out = json.loads(res)
+    # # get output from gentle
+    print('reeeeeeee')
+
+    url = 'http://localhost:8765/transcriptions?async=false'
+    files = {'audio': open(audio, 'rb'),
+             'transcript': open('generate/script.txt', 'rb')}
+    r = requests.post(url, files=files)
+    gentle_out = json.loads(r)
     return gentle_out
