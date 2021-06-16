c483d7a862ec4bddd19f0d76dcaa51cc4596b1ae
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-06-15 20:44:30 -0400

    can add basic poses to json

--- character-creator/build/main.js
+++ character-creator/build/main.js
@@ -1,33 +1,78 @@
+"use strict";
 var dropzone;
-var img;
-var char_made = false;
-var cnv;
-var border = 10;
+let cnv;
+var json_made = false;
+let border = 10;
+let character;
+let img_name;
+let json;
+let mouth_pos = [0, 0];
 function setup() {
-    cnv = createCanvas(500, 500);
-    background(0);
+    cnv = createCanvas(0, 0);
+    cnv.parent("canvas");
+    stroke(0);
+    strokeWeight(1);
     dropzone = select("#dropzone");
     dropzone.dragOver(highlight);
     dropzone.dragLeave(unhighlight);
     dropzone.drop(gotFile, unhighlight);
+    rectMode(CENTER);
 }
 function draw() {
-    if (img) {
+    if (character) {
         fill(200, 0, 0);
-        rect(0, 0, width, height);
-        image(img, border, border);
-        console.log(img.width);
+        rect(0, 0, width * 2, height * 2);
+        image(character, border, border);
+        target(mouth_pos[0], mouth_pos[1]);
     }
 }
 function gotFile(file) {
-    img = createImg(file.data);
-    img.hide();
-    cnv = createCanvas(img.width + 2 * border, img.height + 2 * border);
+    if (file.type === "image") {
+        img_name = file.name;
+        character = createImg(file.data);
+        character.hide();
+        background(0);
+        cnv = createCanvas(character.width + 2 * border, character.height + 2 * border);
+        cnv.parent("canvas");
+    }
+    else if (file.type === "application" && !json_made) {
+        json = file.data;
+        if (json.facesFolder) {
+            alert("JSON Loaded Successfully");
+        }
+        json_made = true;
+    }
 }
 function highlight() {
     dropzone.style("background-color", "#ccc");
 }
 function unhighlight() {
     dropzone.style("background-color", "#fff");
+}
+function mousePressed() {
+    if (border < mouseX &&
+        mouseX < width - border &&
+        border < mouseY &&
+        mouseY < height - border) {
+        mouth_pos = [mouseX, mouseY];
+    }
+}
+function target(x, y) {
+    fill(0, 200, 0);
+    rect(x, y, 40, 3);
+    rect(x, y, 3, 20);
+}
+function addPose() {
+    let gc = new Map();
+    var x = document.getElementById("form").elements;
+    for (const i of x) {
+        gc.set(i.name, i.value);
+    }
+    let pose = {
+        image: img_name,
+        x: mouth_pos[0],
+        y: mouth_pos[1],
+    };
+    json[gc.get("pose_name")] = pose;
 }
 //# sourceMappingURL=../src/src/main.js.map
