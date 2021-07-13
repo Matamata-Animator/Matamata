e45be8db0a63ba8df74696a29f885df5be23a26f
effdotsh <ephram@eff.sh>
2021-07-12 22:48:08 -0400

    Made progress bar utf-8 safe

--- bar.py
+++ bar.py
@@ -6,14 +6,14 @@ import shutil
 def get_gradient(dec):
     gradient = ' '
     if 1 / 8 <= dec < 5 / 8:
-        gradient = '▌'
+        gradient = '#'
     elif 5 / 8 <= dec <= 8 / 8:
-        gradient = '█'
+        gradient = '#'
 
     return gradient
 
 
-def print_bar(numerator, denominator, prefix='', length=shutil.get_terminal_size((80, 20))[0], char='█'):
+def print_bar(numerator, denominator, prefix='', length=shutil.get_terminal_size((80, 20))[0], char='#'):
     numerator = min(numerator, denominator)
     pre = f'{prefix} |'
     length -= 2 * len(str(denominator)) + 15 + len(pre)
