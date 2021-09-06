1947d080fb37242f3d16ef782f94fce8e737ac6e
effdotsh <ephram@eff.sh>
2021-09-06 17:38:29 -0400

    gets output from gentle

--- src/gentle.ts
+++ src/gentle.ts
@@ -1,12 +1,25 @@
-// import fs from "fs";
-
-import { curly, Curl } from "node-libcurl";
+import { rejects } from "assert/strict";
+import { Curl } from "node-libcurl";
+import { resolve } from "path/posix";
 
 let url = "http://localhost:8765/transcriptions?async=false";
 
 export async function gentleAlign(audio_path: string, script_path: string) {
-  //   const form = new FormData();
-  //   form.append("audio", fs.createReadStream(audio_path));
-  //   form.append("transcript", fs.createReadStream(script_path));
-  //   axios.post(url, form, { headers: form.getHeaders() });
+  let gentle_out = new Promise<unknown>((resolve, reject) => {
+    const curl = new Curl();
+    const close = curl.close.bind(curl);
+
+    curl.setOpt(Curl.option.URL, url);
+    curl.setOpt(Curl.option.HTTPPOST, [
+      { name: "audio", file: audio_path },
+      { name: "transcript", file: script_path },
+    ]);
+
+    curl.on("end", function (statusCode, data, headers) {
+      resolve(data);
+    });
+    curl.on("error", close);
+    curl.perform();
+  });
+  return gentle_out;
 }
