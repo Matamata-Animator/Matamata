167c90690f9e1637b67d19c35d185e1394b07f74
AI-Spawn <aispawn@aispawn.com>
2023-11-25 13:33:08 -0700

    argparser

--- matamata.go
+++ matamata.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-// "os"
-// "fmt"
+	"fmt"
 )
 
-func main() {
-	parseArgs()
-	//_agrs := parseArgs()
+var loglevel int8 = 0
 
-	//downloadModel()
+func logM(message any, level int8) {
+	if loglevel >= level {
+		fmt.Println(message)
+	}
+}
+
+func main() {
+	args := parseArgs()
+	logM(args, 3)
+
+	downloadModel()
 	//
 	//fmt.Println("Done!")
 }
