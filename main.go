fb50b3aedeb8c1e2960b9f50cbec25ec4109e5f1
effdotsh <ephram@eff.sh>
2023-11-25 18:59:32 -0700

    gentle align works

--- main.go
+++ main.go
@@ -6,20 +6,30 @@ import (
 
 var loglevel int8 = 1
 
-func logM(message any, level int8) {
+func logM(level int8, message ...any) {
 	if loglevel >= level {
-		fmt.Println(message)
+		fmt.Println(message...)
 	}
+	fmt.Println()
 }
 
 func main() {
 	args := parseArgs()
-	logM(args, 3)
+	logM(3, "Args:", args)
 
 	//downloadModel()
+	logM(1, "Transcribing Audio...")
 
-	text := transcribe(args.audioPath, args.transcriberURL, args.transcriberApiKey)
-	logM("Transcription: ", 2)
-	logM(text, 2)
+	//text := transcribe(args.audioPath, args.transcriberUrl, args.transcriberApiKey)
+	//cache transcription to save time durinng development
+	text := "That quick beige fox jumped in the air over each thin dog. Look out, I shout, for he's foiled you again, creating chaos."
 
+	logM(2, "Transcription:", text)
+	logM(1, "Aligning Audio...")
+
+	gentleResponse, err := gentleAlign(args.alignerUrl, args.audioPath, text)
+	if err != nil {
+		panic(err)
+	}
+	logM(3, gentleResponse)
 }
