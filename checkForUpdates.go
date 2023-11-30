53ca08584c11bf109ad3bbdf4a6b875b888b388a
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2023-11-30 01:46:37 -0700

    Fixed negative duration bug on linux

--- checkForUpdates.go
+++ checkForUpdates.go
@@ -16,7 +16,7 @@ type Release struct {
 }
 
 func checkForUpdates() {
-	currentSemVer := "v5.0.0"
+	currentSemVer := "v5.0.1"
 
 	logM(1, "Checking for Updates...")
 	endpointUrl := "https://api.github.com/repos/Matamata-Animator/Matamata/releases?per_page=1"
