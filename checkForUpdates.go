4ec736ee4feb3ac3530aac2fc226d56834a88a8a
AI-Spawn <aispawn@aispawn.com>
2023-12-03 01:27:43 -0700

    increment version 5.0.2->5.0.3

--- checkForUpdates.go
+++ checkForUpdates.go
@@ -16,7 +16,7 @@ type Release struct {
 }
 
 func checkForUpdates() {
-	currentSemVer := "v5.0.2"
+	currentSemVer := "v5.0.3"
 
 	logM(1, "Checking for Updates...")
 	endpointUrl := "https://api.github.com/repos/Matamata-Animator/Matamata/releases?per_page=1"
